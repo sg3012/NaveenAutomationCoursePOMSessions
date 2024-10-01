189c539f08559aef7b8a0c2b25062fa7eae49693
Shubham Gupta4 <Shubham.Gupta4@insight.com>
2024-10-01 17:58:30 +0530

    updated cartpage as per NaveenTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
-	String product = "macbook";
+	By product = By.id("productId");
 	
+	public void clickProduct() {
+		System.out.println("click on macbook");
+	}
 }
