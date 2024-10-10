161bb47dfe68b0bbcf3c546bbe43f8fceeadc8b5
ShubhamQa <sgnight30@gmail.com>
2024-10-10 19:54:58 +0530

    Update User.java

--- src/main/java/com/qa/opencart/pages/User.java
+++ src/main/java/com/qa/opencart/pages/User.java
@@ -4,11 +4,13 @@ public class User {
 
 	public void m1() {
 		double d = 12.33;
-		double d2 = 23.44;
+		double d2 = 23.77;
+		double d3 = 15.33;
 	}
 
 	public int m2() {
 		String p1 = "selenium";
+		int p2 = 45;
 		return 123;
 	}
 }
